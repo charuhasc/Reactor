d485990b4751d90780468d167135faa345cf49bc
charuhasc <tafelmusik.ch@gmail.com>
2024-03-09 21:10:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div>
+      <Profile></Profile>
+      {/* <Profile></Profile>
+      <Profile></Profile> */}
+    </div>
+  )
+
+}
+
+export default App;
